857dbe29788abe78f7fc16ca144b5f01167aba6f
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2025-01-16 17:32:07 -0300

    adjusting cart provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./Router";
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+import { CartContextProvider } from './contexts/CartProvider';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-
+        <CartContextProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </CartContextProvider>
         <GlobalStyle />
       </ThemeProvider>
     </>
